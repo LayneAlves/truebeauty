5159931f7793354e8f936caa6d467eef6d5caea4
layne <layne.silva1007@gmail.com>
2025-09-24 20:13:13 -0300

    Organizei imagens em pastas. Criei a pag Cabelos

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -5,10 +5,12 @@ const router = express.Router();
 const indexController = require('../controllers/indexController');
 const vertudoController = require('../controllers/vertudoController');
 const perfumesController = require('../controllers/perfumesController');
+const cabelosController = require('../controllers/cabelosController');
 
 // ROUTER
 router.get('/', indexController.renderIndex);
 router.get('/vertudo', vertudoController.renderVertudo);
 router.get('/perfumes', perfumesController.renderPerfumes);
+router.get('/cabelos', cabelosController.renderCabelos);
 
 module.exports = router;
