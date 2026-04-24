0edd3873a29cdf65940570e30657f2448e694a0f
Layne <layne.silva1007@gmail.com>
2026-04-24 12:29:05 -0300

    Cria a validação do formulário da pag de checkout

--- app/public/assets/js/validacoes.js
+++ app/public/assets/js/validacoes.js
@@ -1,10 +1,26 @@
 // VALIDAÇÃO NOME
 addEventListener("DOMContentLoaded", () => {
     const fields = {
-        nome: document.querySelector("input[name='nome']"),
+        nome: document.querySelectorAll("input[name='nome']"),
         nomeError: document.querySelector("#nome-error"),
         email: document.querySelectorAll("input[name='email']"),
         emailError: document.querySelector("#email-error"),
+        telefone: document.querySelectorAll("input[name='telefone']"),
+        telefoneError: document.querySelector("#telefone-error"),
+        cep: document.querySelectorAll("input[name='cep']"),
+        cepError: document.querySelector("#cep-error"),
+        endereco: document.querySelectorAll("input[name='endereco']"),
+        enderecoError: document.querySelector("#endereco-error"),
+        numero: document.querySelectorAll("input[name='numero']"),
+        numeroError: document.querySelector("#numero-error"),
+        complemento: document.querySelectorAll("input[name='complemento']"),
+        complementoError: document.querySelector("#complemento-error"),
+        bairro: document.querySelectorAll("input[name='bairro']"),
+        bairroError: document.querySelector("#complemento-bairro"),
+        cidade: document.querySelectorAll("input[name='cidade']"),
+        cidadeError: document.querySelector("#complemento-cidade"),
+        estado: document.querySelectorAll("input[name='estado']"),
+        estadoError: document.querySelector("#complemento-estado"),
         senha: document.querySelectorAll("input[name='senha']"),
         senhaError: document.querySelector("#senha-error"),
     }
@@ -16,22 +32,25 @@ addEventListener("DOMContentLoaded", () => {
         special: document.querySelector("#req-special"),
     }
 
-    if (fields.nome) {
+    // VALIDAÇÃO NOME
+    fields.nome.forEach(field => {
+        if (field) {
+            field.addEventListener("input", () => {
+                let value = field.value.trim();
+                if (value.length >= 3) {
+                    field.classList.remove("invalid");
+                    field.classList.add("valid");
+                    fields.nomeError.style.display = 'none';
+                } else {
+                    field.classList.remove("valid");
+                    field.classList.add("invalid");
+                    fields.nomeError.style.display = 'flex';
+                }
+            });
+        }
+    });
 
-        fields.nome.addEventListener("input", () => {
-            let value = fields.nome.value.trim();
-            if (value.length >= 3) {
-                fields.nome.classList.remove("invalid");
-                fields.nome.classList.add("valid");
-                fields.nomeError.style.display = 'none';
-            } else {
-                fields.nome.classList.remove("valid");
-                fields.nome.classList.add("invalid");
-                fields.nomeError.style.display = 'flex';
-            }
-        });
-    }
-
+    // VALIDAÇÃO EMAIL
     fields.email.forEach(field => {
         if (field) {
             field.addEventListener("input", () => {
@@ -52,6 +71,145 @@ addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    // VALIDAÇÃO NÚMERO DE TELEFONE
+    fields.telefone.forEach(field => {
+        if (field) {
+            field.addEventListener("input", () => {
+                let value = field.value.trim();
+                if (value.length >= 12) {
+                    field.classList.remove("invalid");
+                    field.classList.add("valid");
+                    fields.telefoneError.style.display = 'none';
+                } else {
+                    
+                    field.classList.remove("valid");
+                    field.classList.add("invalid");
+                    fields.telefoneError.style.display = 'flex';
+                }
+            });
+        }
+    });
+    // VALIDAÇÃO CEP
+    fields.cep.forEach(field => {
+        if (field) {
+            field.addEventListener("input", () => {
+                let value = field.value.trim();
+                if (value.length >= 9) {
+                    field.classList.remove("invalid");
+                    field.classList.add("valid");
+                    fields.cepError.style.display = 'none';
+                } else {
+                    field.classList.remove("valid");
+                    field.classList.add("invalid");
+                    fields.cepError.style.display = 'flex';
+                }
+            });
+        }
+    });
+    
+    // VALIDAÇÃO ENDEREÇO 
+    fields.endereco.forEach(field => {
+        if (field) {
+            field.addEventListener("input", () => {
+                let value = field.value.trim();
+                if (value.length >= 5) {
+                    field.classList.remove("invalid");
+                    field.classList.add("valid");
+                    fields.enderecoError.style.display = 'none';
+                } else {
+                    field.classList.remove("valid");
+                    field.classList.add("invalid");
+                    fields.enderecoError.style.display = 'flex';
+                }
+            });
+        }
+    });
+    // VALIDAÇÃO NÚMERO 
+    fields.numero.forEach(field => {
+        if (field) {
+            field.addEventListener("input", () => {
+                let value = field.value.trim();
+                if (value.length >= 2) {
+                    field.classList.remove("invalid");
+                    field.classList.add("valid");
+                    fields.numeroError.style.display = 'none';
+                } else {
+                    field.classList.remove("valid");
+                    field.classList.add("invalid");
+                    fields.numeroError.style.display = 'flex';
+                }
+            });
+        }
+    });
+    // VALIDAÇÃO COMPLEMENTO 
+    fields.complemento.forEach(field => {
+        if (field) {
+            field.addEventListener("input", () => {
+                let value = field.value.trim();
+                if (value.length >= 4) {
+                    field.classList.remove("invalid");
+                    field.classList.add("valid");
+                    fields.complementoError.style.display = 'none';
+                } else {
+                    field.classList.remove("valid");
+                    field.classList.add("invalid");
+                    fields.complementoError.style.display = 'flex';
+                }
+            });
+        }
+    });
+    // VALIDAÇÃO BAIRRO 
+    fields.bairro.forEach(field => {
+        if (field) {
+            field.addEventListener("input", () => {
+                let value = field.value.trim();
+                if (value.length >= 10) {
+                    field.classList.remove("invalid");
+                    field.classList.add("valid");
+                    fields.bairroError.style.display = 'none';
+                } else {
+                    field.classList.remove("valid");
+                    field.classList.add("invalid");
+                    fields.bairroError.style.display = 'flex';
+                }
+            });
+        }
+    });
+    // VALIDAÇÃO CIDADE 
+    fields.cidade.forEach(field => {
+        if (field) {
+            field.addEventListener("input", () => {
+                let value = field.value.trim();
+                if (value.length >= 10) {
+                    field.classList.remove("invalid");
+                    field.classList.add("valid");
+                    fields.cidadeError.style.display = 'none';
+                } else {
+                    field.classList.remove("valid");
+                    field.classList.add("invalid");
+                    fields.cidadeError.style.display = 'flex';
+                }
+            });
+        }
+    });
+    // VALIDAÇÃO ESTADO 
+    fields.estado.forEach(field => {
+        if (field) {
+            field.addEventListener("input", () => {
+                let value = field.value.trim();
+                if (value.length >= 2) {
+                    field.classList.remove("invalid");
+                    field.classList.add("valid");
+                    fields.estadoError.style.display = 'none';
+                } else {
+                    field.classList.remove("valid");
+                    field.classList.add("invalid");
+                    fields.estadoError.style.display = 'flex';
+                }
+            });
+        }
+    });
+    
     fields.senha.forEach(field => {
         if (field) {
             field.addEventListener("input", () => {
