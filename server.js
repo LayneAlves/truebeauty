0c50989b06c62790c1d885ae83236abc357fb1cd
Layne <layne.silva1007@gmail.com>
2026-05-07 21:06:24 -0300

    Testa hospedagem

--- server.js
+++ server.js
@@ -3,23 +3,27 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-// app.locals.baseUrl = 'http://localhost:3000'; Teste, para ver se o css roda no navegador 
-app.locals.baseUrl = ''; 
-app.set('view engine', 'ejs');
+app.use((req, res, next) => {
+    const protocol = req.headers['x-forwarded-proto'] || req.protocol;
+    const host = req.get('host');
+
+    app.locals.baseUrl = `${protocol}://${host}`;
+    next(); 
+});
+
+app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, 'app/views'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser()); 
+app.use(cookieParser());
 
-// app.use(express.static(path.join(__dirname, 'app/public'))); Teste, para ver se o css roda no navegador
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 const indexRoutes = require('./app/routes/routes');
 app.use('/', indexRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em port ${PORT}`);
 });
-
