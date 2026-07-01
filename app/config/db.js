5a324d0501eef9a2d03610939412c76ead9e2201
Layne <layne.silva1007@gmail.com>
2026-06-30 21:50:52 -0300

    Adiciona variavel de ambiente

--- app/config/db.js
+++ app/config/db.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
+require("dotenv").config();
 
 const pool = mysql.createPool({
-    host: '216.172.172.19',
-    port: 3306,
-    user: 'danil458_truebeauty',
-    password: '@ITBtruebeauty',
-    database: 'danil458_truebeautyBD',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10
 });
